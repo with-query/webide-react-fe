f4da29c08cd323755e1d671a78a3917c4d8500a1
odihan <odinia@hanyang.ac.kr>
2025-08-01 01:57:33 +0900

    Delete chatroom_chakraUI/src directory

--- chatroom_chakraUI/src/chat/index.ts
+++
@@ -1,6 +0,0 @@
-// 채팅 컴포넌트 통합 export 파일
-export { default as ChatProvider } from './ChatProvider';
-export { default as ChatContainer } from './ChatContainer';
-export { default as ChatMessage } from './ChatMessage';
-export { default as ChatMessageList } from './ChatMessageList';
-export { default as ChatInput } from './ChatInput';
